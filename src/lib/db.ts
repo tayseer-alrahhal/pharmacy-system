fdd8dc2c7ce47c541d2fa37fed82806eb6d9b7ef
tayseer-alrahhal <tayseer.alrahhal.dev@gmail.com>
2026-02-23 00:04:52 +0300

    feat: Add MongoDB connection utility using Mongoose.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb://localhost:27017/pharmacy";
+// const MONGODB_URI = "mongodb://localhost:27017/pharmacy";
+const MONGODB_URI = "mongodb+srv://learnova:VifRxi1st3h1Epi6@learnova.dfptnye.mongodb.net/pharmacy-system?retryWrites=true&w=majority&appName=pharmacy-system";
 
 export async function connectDB() {
     if (mongoose.connection.readyState >= 1) return;
